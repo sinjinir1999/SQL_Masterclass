9642bb25f551d036fe82fadbf0cc237338cd2818
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:25:16 +0530

    Update 8.sql

--- 8.sql
+++ 8.sql
@@ -1,3 +1,13 @@
+> How many mentors are there per region? Sort the output by regions with the most mentors to the least
+
+|    region     | mentor_count |
+| ------------- | ------------ |
+| United States |            7 |
+| Australia     |            4 |
+| India         |            1 |
+| Africa        |            1 |
+| Asia          |            1 |
+
 SELECT region
 	,COUNT(DISTINCT member_id) AS mentor_count
 FROM trading.members
