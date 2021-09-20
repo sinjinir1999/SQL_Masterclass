d7d7f95e2647f133e1fe91a40c7e9685a56afe32
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:18:05 +0530

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,3 +1,5 @@
+> Return the unique `region` values from the `trading.members` table and sort the output by reverse alphabetical order
+
 SELECT DISTINCT region
 FROM trading.members
 ORDER BY region DESC
