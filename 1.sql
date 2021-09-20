4575bfc12a23ae16e4c454aab47a7e5fca16d9b8
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:24:25 +0530

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,4 +1,12 @@
 > Show only the top 5 rows from the `trading.members` table
 
+| member_id | first_name |    region     |
+| --------- | ---------- | ------------- |
+| c4ca42    | Danny      | Australia     |
+| c81e72    | Vipul      | United States |
+| eccbc8    | Charlie    | United States |
+| a87ff6    | Nandita    | United States |
+| e4da3b    | Rowan      | United States |
+
 SELECT *
 FROM trading.members LIMIT 5
