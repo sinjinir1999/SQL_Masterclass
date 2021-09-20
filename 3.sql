0dd533a6c2786e791d8e0aa06d7d2351cf21a12b
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:23:07 +0530

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,5 +1,15 @@
 > Which records from `trading.members` are from the United States region?
 
+| member_id | first_name |    region     |
+| --------- | ---------- | ------------- |
+| c81e72    | Vipul      | United States |
+| eccbc8    | Charlie    | United States |
+| a87ff6    | Nandita    | United States |
+| e4da3b    | Rowan      | United States |
+| 167909    | Ayush      | United States |
+| 8f14e4    | Alex       | United States |
+| c9f0f8    | Abe        | United States |
+
 SELECT *
 FROM trading.members
 WHERE region = 'United States'
