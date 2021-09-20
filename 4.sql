180c99109d50d79d79f48582ee1ac6828143d6f1
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:20:21 +0530

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,5 +1,18 @@
 > Select only the `member_id` and `first_name` columns for members who are not from Australia
 
+| member_id | first_name |
+| --------- | ---------- |
+| c81e72    | Vipul      |
+| eccbc8    | Charlie    |
+| a87ff6    | Nandita    |
+| e4da3b    | Rowan      |
+| 167909    | Ayush      |
+| 8f14e4    | Alex       |
+| c9f0f8    | Abe        |
+| d3d944    | Enoch      |
+| 6512bd    | Vikram     |
+| c20ad4    | Leah       |
+
 SELECT member_id
 	,first_name
 FROM trading.members
