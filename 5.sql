639a065e70dcb96311e4d7718ba856607765cf93
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:19:57 +0530

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,5 +1,13 @@
 > Return the unique `region` values from the `trading.members` table and sort the output by reverse alphabetical order
 
+|    region     | 
+| ------------- |
+| United States |
+| India         |
+| Australia     |
+| Asia          |
+| Africa        |
+
 SELECT DISTINCT region
 FROM trading.members
 ORDER BY region DESC
