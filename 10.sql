381498e90e8931810414b8c4a3497b3554a651db
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:26:15 +0530

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -1,3 +1,10 @@
+
+> How many mentors have a first name starting with a letter before `'E'`?
+
+| mentor_count |
+| -------------|
+|            6 |
+
 SELECT COUNT(DISTINCT member_id) AS mentor_count
 FROM trading.members
 WHERE LEFT(first_name, 1) < 'E'
