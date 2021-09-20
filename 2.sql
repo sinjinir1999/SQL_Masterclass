f680d58298602427ae378804537d0869e013d4d5
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-20 12:23:29 +0530

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,5 +1,12 @@
 > Sort all the rows in the table by `first_name` in alphabetical order and show the top 3 rows
 
+
+| member_id | first_name |    region     |
+| --------- | ---------- | ------------- |
+| c9f0f8    | Abe        | United States |
+| 8f14e4    | Alex       | United States |
+| 167909    | Ayush      | United States |
+
 SELECT *
 FROM trading.members
 ORDER BY first_name LIMIT 3;
